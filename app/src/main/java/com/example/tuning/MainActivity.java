164f1d4493df917ea15529f7ac5b0998e10954a2
Mashrafi <gaintakwa@gmail.com>
2023-01-23 17:02:37 +0600

    Front page Creation

--- app/src/main/java/com/example/tuning/MainActivity.java
+++ app/src/main/java/com/example/tuning/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tuning;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,6 +12,8 @@ import android.app.admin.FactoryResetProtectionPolicy;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,57 @@ public class MainActivity extends AppCompatActivity {
     private void initViewPager() {
         ViewPager viewPager = findViewById(R.id.viewpager);
         TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new SongsFragment(),"Songs");
+        viewPagerAdapter.addFragments(new AlbumFragment(),"Albums");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        void addFragments(Fragment fragment,String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        //Return the Fragment associated with a specified position.
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+
+        //Return the number of views available.
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        /**
+         * This method may be called by the ViewPager to obtain a title string
+         * to describe the specified page. This method may return null
+         * indicating no title for this page. The default implementation returns
+         * null.
+         *
+         * @param position The position of the title requested
+         * @return A title for the requested page
+         */
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
