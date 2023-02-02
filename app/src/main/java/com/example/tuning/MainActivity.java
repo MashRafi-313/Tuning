64603e928405601da43790c3d5194e4e5506b9ad
Mashrafi <gaintakwa@gmail.com>
2023-02-02 19:04:59 +0600

    user-permission added

--- app/src/main/java/com/example/tuning/MainActivity.java
+++ app/src/main/java/com/example/tuning/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.tuning;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
 import android.app.admin.FactoryResetProtectionPolicy;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -17,11 +23,42 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permission();
         initViewPager();
+    }
+
+
+    private void permission() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!=
+        PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]
+                {Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+        }
+        else{
+            Toast.makeText(this,"permission granted",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE){
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                //DO whatever i want
+                Toast.makeText(this,"permission granted",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]
+                        {Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+            }
+        }
+
     }
 
     private void initViewPager() {
@@ -33,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
     }
+
+
 
     public static class ViewPagerAdapter extends FragmentPagerAdapter{
 
