abe0d25dc908183ed4894a106b27014b79ed1cc3
Mashrafi <gaintakwa@gmail.com>
2023-02-18 00:37:09 +0600

    albumAdapter created

--- app/src/main/java/com/example/tuning/AlbumFragment.java
+++ app/src/main/java/com/example/tuning/AlbumFragment.java
@@ -1,20 +1,20 @@
 package com.example.tuning;
 
+import static com.example.tuning.MainActivity.musicFiles;
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlbumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AlbumFragment extends Fragment {
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+public class AlbumFragment extends Fragment {
+    RecyclerView recyclerView;
+    AlbumAdapter albumAdapter;
 
     public AlbumFragment() {
         // Required empty public constructor
@@ -28,7 +28,14 @@ public class AlbumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_album, container, false);
+        View view = inflater.inflate(R.layout.fragment_album, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        if(!(musicFiles.size() < 1)){
+            albumAdapter = new AlbumAdapter(getContext(),musicFiles);
+            recyclerView.setAdapter(albumAdapter);
+            recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        }
         return view;
     }
 }
